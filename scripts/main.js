b5d69f7fafaffa92d023d3f163ccacc95c1ba944
Divyansh Jain <djboss8834@gmail.com>
2025-06-08 09:53:00 +0530

    REMOVE CONSOLE

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-console.log("API KEY:", apiKey); 
 if (!apiKey) {
   document.getElementById('map').innerHTML = '<p>Error: Unable to load map. Please contact support.</p>';
 } else {
@@ -58,6 +57,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-console.log("ENV:", import.meta.env);
-console.log("API KEY:", import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
